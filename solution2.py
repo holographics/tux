b270744c6e0819df4ed439705ca487fe97b6f865
Pasha <43281113+holographics@users.noreply.github.com>
2019-05-25 15:41:36 -0700

    Update solution2.py

--- solution2.py
+++ solution2.py
@@ -1,17 +1,16 @@
-
 from collections import defaultdict
-events = dict(A=defaultdict(list), 
+orders = dict(A=defaultdict(list), 
             B=defaultdict(list))
 
 def place_order(small, medium, large, day):
-    for style, data in events.items():
+    for style, events in orders.items():
         total = [small, medium, large]
         for i in range(day-2, day+1):
-            for rent in data[i]:
-                total = map(sum, zip(total, rent))
+            for event in events[i]:
+                total = map(sum, zip(total, event))
 
         if all([n<=5 for n in total]):
-            events[style][day].append([small, medium, large])
+            orders[style][day].append([small, medium, large])
             print 'Sufficient number of tuxedos is available on day %s' % day
             break 
     else: 
