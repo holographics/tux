85f9b01c4641a73012296c82cc1d97466759624a
Pasha <43281113+holographics@users.noreply.github.com>
2019-06-03 10:31:13 -0700

    Create solution3.py

---
+++ solution3.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+from collections import defaultdict
+
+orders = dict(A=defaultdict(list), 
+            B=defaultdict(list))
+
+
+def check_count(total, days, day, count): 
+    for order in days[day]:
+        total = list(map(sum, zip(order, total)))
+
+    if day == count-2:
+        return total
+    return check_count(total, days, day-1, count)
+            
+
+def place_order(small, medium, large, day):
+    order = [small, medium, large]
+    for style, days in orders.items():
+        total = check_count(order, days, day, day)
+        
+        if all([n<=5 for n in total]):
+            orders[style][day].append(order)
+            return True 
+    return False 
+
+
+if __name__ == '__main__': 
+    for day in range(1, 5):
+        res = place_order(small = 3, medium = 3, large = 3, day = 2)
+        if res:
+            print ('Sufficient number of tuxedos is available on day %s' % day)
+        else:
+            print ('More tuxedos are needed for day %s!' % day)
